2df879689689b68313eb10ffc0f03f64d7fe5326
Gopu k <92145286+Gopuk@users.noreply.github.com>
2024-06-24 22:36:19 +0530

    added forms

---
+++ tracker/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Entry
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['date', 'description', 'amount', 'is_expense']
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password']
