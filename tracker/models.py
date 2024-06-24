3bcae08956fd53b050a60f8e1775e68311acb1fd
Gopu k <92145286+Gopuk@users.noreply.github.com>
2024-06-24 22:33:57 +0530

    added_models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Entry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    is_expense = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.description} - {self.amount}"
